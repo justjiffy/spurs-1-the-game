7232c95985c2de1d532ee37d094409124cd83799
Juli Emmel <justjiffy@gmail.com>
2015-08-23 10:42:16 -0700

    adding board array for game to try and hone in on piece movement logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+//global variables(?)... I couldn't call these in inspector when they were inside the $(function() {}); tags
+
+var array = [1,2,3,4];
+
+var boardArray = [["X", "X", "X", "Start", "", "", "", "X", "X", "X"], //row 1
+			  [ "X", "X", "X", "", "X", "X", "", "X", "X", "X" ], //row 2	
+			  [ "X", "X", "X", "", "X", "X", "", "X", "X", "X" ], //row 3
+			  ["X", "X", "X", "Hospital", "", "", "", "X", "X", "X"]];
+
+
+//game code
+$(function() {
+
+
+
+console.log(boardArray);
+
 $('#showL2').on('click', function() { $('#level2').show(); });
 $('#showL3').on('click', function() { $('#level3').show(); });
 $('#showL4').on('click', function() { $('#level4').show(); });
@@ -18,3 +35,7 @@ $('body').keyup(function(el) {
 var dieRoll = function() {
 	$('#die').html((Math.floor((Math.random()*6)+1)))
 };
+
+$('aside').draggable();
+$('aside').resizable();
+  });
