e217207f129a419aab02da6f66c57b87dd8164f9
Juli Emmel <justjiffy@gmail.com>
2015-08-24 11:53:09 -0700

    created a an array for level one that you can right arrow through once

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 $(function() {	
+
+Array.prototype.next = function() {
+    return this[++this.current];
+};
+Array.prototype.prev = function() {
+    return this[--this.current];
+};
+Array.prototype.current = 0;
 	
 boardArray = [["X", "X", "X", "S", "Z", "", "", "X", "X", "X"], //row 1  (s = start)
 			  [ "X", "X", "X", "", "X", "X", "", "X", "X", "X" ], //row 2	
@@ -24,15 +32,33 @@ boardArray = [["X", "X", "X", "S", "Z", "", "", "X", "X", "X"], //row 1  (s = st
 			  [ "X", "X", "X", "", "X", "X", "", "X", "X", "X"], //row 22
 			  [ "X", "X", "X", "", "", "", "F", "X", "X", "X"]]; //row 23   (f = finish);
 
-	
-	var makeMove = function(id, row, index) { 
-			$(id).children()[index].innerHTML = "<img src='images/p1-sherrif.png' />";
-	};
+rightCount = 0;
 
-	$('html').on('keyup', function(el) {
-	if (el.keyCode == 39) {
-		makeMove('#level1', 0, 3); }
-	});
+l1Array = [3, 4, 5, 6, 16, 26, 36, 35, 34, 33, 23, 13];
+p1location = l1Array[0];
+
+var start = function() {;
+		$('#level1').children()[p1location].innerHTML = "<img src='images/p1-sherrif.png' />";
+};
+
+
+var moveRight = function() {
+		$('#level1').children()[p1location].innerHTML = "";
+		p1location = l1Array.next(); 
+		$('#level1').children()[p1location].innerHTML = "<img src='images/p1-sherrif.png' />";
+		if (p1location == 13) { p1location = l1Array[0] };	
+};
+
+$('html').on('keyup', function(el) {
+	if (rightCount === 0 && el.keyCode == 39) {
+			start();
+			rightCount++; 
+	}
+	else if (rightCount > 0 && el.keyCode == 39) {
+			rightCount++;
+			return moveRight();
+	}
+});
 
 $('#showL2').on('click', function() { $('#level2').show(); });
 $('#showL3').on('click', function() { $('#level3').show(); });
