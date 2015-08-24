24b7f4bfe6bcdae423f75a9512b0aa8d9afc01ce
Juli Emmel <justjiffy@gmail.com>
2015-08-23 17:41:03 -0700

    created p1 game piece. appears at start when you push right arrow

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
-//global variables(?)... I couldn't call these in inspector when they were inside the $(function() {}); tags
-
-var array = [1,2,3,4];
-
-var boardArray = [["X", "X", "X", "Start", "", "", "", "X", "X", "X"], //row 1
+$(function() {	
+	
+boardArray = [["X", "X", "X", "S", "Z", "", "", "X", "X", "X"], //row 1  (s = start)
 			  [ "X", "X", "X", "", "X", "X", "", "X", "X", "X" ], //row 2	
 			  [ "X", "X", "X", "", "X", "X", "", "X", "X", "X" ], //row 3
-			  ["X", "X", "X", "Hospital", "", "", "", "X", "X", "X"], //row 4
+			  ["X", "X", "X", "H", "", "", "", "X", "X", "X"], //row 4      (h = hospital)
 			  ["X", "X", "X", "X", "X", "X", "", "X", "X", "X"], //row 5
 			  ["X", "X", "X", "X", "X", "X", "", "X", "X", "X"], //row 6
 			  ["X", "X", "X", "X", "", "", "", "", "X", "X"], //row 7
@@ -24,13 +22,17 @@ var boardArray = [["X", "X", "X", "Start", "", "", "", "X", "X", "X"], //row 1
 			  ["X", "X", "X", "", "", "", "", "X", "X", "X"], //row 20
 			  [ "X", "X", "X", "", "X", "X", "", "X", "X", "X"], //row 21
 			  [ "X", "X", "X", "", "X", "X", "", "X", "X", "X"], //row 22
-			  [ "X", "X", "X", "", "", "", "Finish", "X", "X", "X"]]; //row 23;
-//game code
-$(function() {
+			  [ "X", "X", "X", "", "", "", "F", "X", "X", "X"]]; //row 23   (f = finish);
 
+	
+	var makeMove = function(id, row, index) { 
+			$(id).children()[index].innerHTML = "<img src='images/p1-sherrif.png' />";
+	};
 
-
-console.log(boardArray);
+	$('html').on('keyup', function(el) {
+	if (el.keyCode == 39) {
+		makeMove('#level1', 0, 3); }
+	});
 
 $('#showL2').on('click', function() { $('#level2').show(); });
 $('#showL3').on('click', function() { $('#level3').show(); });
@@ -38,14 +40,19 @@ $('#showL4').on('click', function() { $('#level4').show(); });
 $('#reset').on('click', function() { $( "#level2, #level3, #level4" ).hide(); });
 
 $('html').on('keydown', function(el) {
-		if(el.keyCode == 32) {
+		if(el.keyCode == 32 ||
+		   el.keyCode == 37 ||
+		   el.keyCode == 38 ||
+		   el.keyCode == 39 ||
+		   el.keyCode == 40
+		   ) {
 			el.preventDefault();
 		}
 });
 
 $('body').keyup(function(el) {
 	if(el.keyCode == 32) {
-		console.log(dieRoll()) 
+		return dieRoll(); 
 	}
 });
 
@@ -53,6 +60,6 @@ var dieRoll = function() {
 	$('#die').html((Math.floor((Math.random()*6)+1)))
 };
 
-$('aside').draggable();
-$('aside').resizable();
+$('aside').draggable(); 
+$('aside').resizable(); //can't be draggable and resizable?? 
   });
